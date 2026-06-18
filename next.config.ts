1fe219c7a6eabd06e7aef2551206738e1de3a530
Zenitsu <hsn.dhiu@gmail.com>
2026-06-18 15:38:38 +0530

    Update and rename next.config.ts to next.config.js

--- next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  allowedDevOrigins: ["*.replit.dev", "*.repl.co", "*.sisko.replit.dev", "*.pike.replit.dev"],
-};
-
-export default nextConfig;
